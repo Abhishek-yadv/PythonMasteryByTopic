cfbbb474b332c1cf3fc13d5aa881626ec956d8ab
Abhishek Yadav <68497250+Abhishek-yadv@users.noreply.github.com>
2024-07-03 23:26:53 +0530

    Add files via upload

--- Python_Lambda.py
+++ Python_Lambda.py
@@ -148,11 +148,180 @@ print(func("apple", "a"))
 
 ###################################################################
 """
-8. Write a Python program to extract year, month, date and time using Lambda.
-Sample Output:
-2020-01-15 09:03:32.744178
-2020
-1
-15
-09:03:32.744178
+9. Write a Python program to check whether a given string is a number or not using Lambda.
 """
+func = lambda strng: True if strng.isnumeric() else False
+print(func("abhs"))
+
+
+###################################################################
+"""
+10. Write a Python program to create Fibonacci series up to n using Lambda.
+Fibonacci series upto 2:
+[0, 1]
+Fibonacci series upto 5:
+[0, 1, 1, 2, 3]
+"""
+from functools import reduce
+fib_func = fib_series = lambda n: reduce(lambda x, _: x + [x[-1] + x[-2]], range(n - 2), [0, 1])
+print(fib_func(0))
+
+###################################################################
+"""
+11. Write a Python program to find the intersection of two given arrays using Lambda.
+Original arrays:
+[1, 2, 3, 5, 7, 8, 9, 10]
+[1, 2, 4, 8, 9]
+Intersection of the said arrays: [1, 2, 8, 9]
+"""
+inter_sec = lambda arr1, arr2: [x for x in arr1 if x in arr2]
+print(inter_sec([1, 2, 3, 5, 7, 8, 9, 10],
+                [1, 2, 4, 8, 9]))
+
+###################################################################
+"""
+12. Write a Python program to rearrange positive and negative numbers in a given array using Lambda.
+Original arrays:
+[-1, 2, -3, 5, 7, 8, 9, -10]
+Rearrange positive and negative numbers of the said array:
+[2, 5, 7, 8, 9, -10, -3, -1]
+"""
+lambda_func = lambda lst : [x for x in lst if x > 0] + sorted([x for x in lst if x < 0], reverse = False)
+print(lambda_func([-1, 2, -3, 5, 7, 8, 9, -10]))
+
+
+###################################################################
+"""
+13. Write a Python program to count the even and odd numbers in a given array of integers using Lambda.
+Original arrays:
+[1, 2, 3, 5, 7, 8, 9, 10]
+Number of even numbers in the above array: 3
+Number of odd numbers in the above array: 5
+"""
+lambda_func = lambda lst : \
+"Number of even numbers in the above array: {}\nNumber of odd numbers in the above array: {}"\
+    .format(len([x for x in lst if x % 2 == 0]), len([x for x in lst if x%2 != 0]))
+print(lambda_func([1, 2, 3, 5, 7, 8, 9, 10]))
+
+###################################################################
+"""
+14. Write a Python program to filter a given list to determine if the 
+values in the list have a length of 6 using Lambda.
+Sample Output:
+Monday Friday Sunday
+"""
+# Create a list 'weekdays' containing the names of the days of the week
+weekdays = ['Monday', 'Tuesday', 'Wednesday',
+            'Thursday', 'Friday', 'Saturday', 'Sunday']
+days = filter(lambda day: True if len(day) == 6 else False, weekdays)
+for d in days:
+    print(d)
+
+###################################################################
+"""
+15. Write a Python program to add two given lists using map and lambda.
+Original list:
+[1, 2, 3]
+[4, 5, 6]
+Result: after adding two list
+[5, 7, 9]
+"""
+lst1, lst2 = [1, 2, 3], [4, 5, 6]
+result = list(map(lambda x, y: x + y, lst1, lst2))
+print(result)
+
+###################################################################
+"""
+16. Write a Python program to find the second lowest total marks of any student(s)
+from the given names and marks of each student using lists and lambda.
+Input the number of students, the names and grades of each student.
+Input number of students: 5
+Name: S ROY Grade: 1
+Name: B BOSE Grade: 3
+Name: N KAR Grade: 2
+Name: C DUTTA Grade: 1
+Name: G GHOSH Grade: 1
+Names and Grades of all students:
+[['S ROY', 1.0], ['B BOSE', 3.0], ['N KAR', 2.0], ['C DUTTA', 1.0], ['G GHOSH', 1.0]]
+Second lowest grade: 2.0
+Names:
+N KAR
+"""
+students = []  
+sec_name = []  
+second_low = 0  
+n = int(input("Input number of students: "))
+for _ in range(n):
+    s_name = input("Name: ")
+    score = float(input("Grade: "))
+    students.append([s_name, score])
+print("\nNames and Grades of all students:")
+print(students)
+
+order = sorted(students, key=lambda x: int(x[1]))
+for i in range(n):
+    if order[i][1] != order[0][1]:
+        second_low = order[i][1]
+        break
+print("\nSecond lowest grade: ", second_low)
+
+sec_student_name = [x[0] for x in order if x[1] == second_low]
+sec_student_name.sort() 
+print("\nNames:")
+for s_name in sec_student_name:
+    print(s_name)
+
+###################################################################
+"""
+17. Write a Python program to find numbers divisible by nineteen or thirteen from a list of numbers using Lambda.
+Orginal list:
+[19, 65, 57, 39, 152, 639, 121, 44, 90, 190]
+Numbers of the above list divisible by nineteen or thirteen:
+[19, 65, 57, 39, 152, 190]
+"""
+lambda_func = lambda lst: [x for x in lst if x%19==0 or x%13==0]
+print(lambda_func([19, 65, 57, 39, 152, 639, 121, 44, 90, 190]))
+
+###################################################################
+"""
+18. Write a Python program to find palindromes in a given list of strings using Lambda.
+Orginal list of strings:
+['php', 'w3r', 'Python', 'abcd', 'Java', 'aaa']
+List of palindromes:
+['php', 'aaa']
+"""
+lambda_func = lambda lst: [x for x in lst if (lambda x: x == x[::-1])(x)]
+print(lambda_func(['php', 'w3r', 'Python', 'abcd', 'Java', 'aaa']))
+
+lambda_func = lambda lst : list(filter(lambda x: x == x[::-1], lst))
+print(lambda_func(['php', 'w3r', 'Python', 'abcd', 'Java', 'aaa']))
+
+###################################################################
+"""
+19. Write a Python program to find all anagrams of a string in a given list of strings using Lambda.
+Orginal list of strings:
+['bcda', 'abce', 'cbda', 'cbea', 'adcb']
+Anagrams of 'abcd' in the above string:
+['bcda', 'cbda', 'adcb']
+"""
+lambda_func = lambda lst, ang: [x for x in lst if sorted(ang) == sorted(x)]
+print(lambda_func(['bcda', 'abce', 'cbda', 'cbea', 'adcb'], 'abcd'))
+
+
+###################################################################
+"""
+20. Write a Python program to find the numbers in a given string and store them in a list.
+Afterward, display the numbers that are longer than the length of the list in sorted form.
+Use the lambda function to solve the problem.
+Original string: sdf 23 safs8 5 sdfsd8 sdfs 56 21sfs 20 5
+Numbers in sorted form:
+20 23 56
+"""
+original_str = "sdf 23 safs8 5 sdfsd8 sdfs 56 21sfs 20 5"
+
+lambda_fun = lambda strn: sorted([int(x) for x in strn.split() if x.isdigit() and int(x) > len([int(x) for x in strn.split() if x.isdigit()])])
+
+result = lambda_fun(original_str)
+print("Numbers in sorted form:")
+print(result)
+
