0152f0261bb0ad195fbaf0c73128e2c1e002f099
Abhishek Yadav <68497250+Abhishek-yadv@users.noreply.github.com>
2024-07-08 23:53:55 +0530

    Add files via upload

--- Python_Lambda.py
+++ Python_Lambda.py
@@ -397,4 +397,15 @@ for original, next_bigger in results.items():
     print(f"Original number: {original}")
     print(f"Next bigger number: {next_bigger}")
 
-
+###################################################################
+"""
+26. Write a Python program to sort a given matrix in ascending order according to the sum of its rows using lambda.
+Original Matrix:
+[[1, 2, 3], [2, 4, 5], [1, 1, 1]]
+Sort the said matrix in ascending order according to the sum of its rows
+[[1, 1, 1], [1, 2, 3], [2, 4, 5]]
+Original Matrix:
+[[1, 2, 3], [-2, 4, -5], [1, -1, 1]]
+Sort the said matrix in ascending order according to the sum of its rows
+[[-2, 4, -5], [1, -1, 1], [1, 2, 3]]
+"""
